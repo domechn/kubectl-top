feb3f736b68daa2b8bc858f85b24625bee56ce56
domgoer <814172254@qq.com>
2019-03-27 17:49:55 +0800

    add README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kubectl-top
 solve the problem that using kubectl top pod will appear error
+
+```bash
+git clone https://github.com/domgoer/kubectl-top.git
+cd kubectl-top
+chmod +x solve.sh
+./solve.sh
+```
--- solve.sh
+++ solve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl create configmap influxdb-config --from-file=https://github.com/domgoer/kubectl-top/config.toml  -n kube-system
+kubectl create configmap influxdb-config --from-file=config.toml  -n kube-system
 
-kubectl apply -f https://github.com/domgoer/kubectl-top/resource.yaml
+kubectl apply -f resource.yaml
